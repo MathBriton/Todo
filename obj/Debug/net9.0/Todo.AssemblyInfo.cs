4ca95b5df8db9116c9bb44da7c341f4be283f24e
MathBriton <mathhbrito@gmail.com>
2025-01-31 15:44:11 -0300

    Subindo Migrations e EF

--- obj/Debug/net9.0/Todo.AssemblyInfo.cs
+++ obj/Debug/net9.0/Todo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee92668387baf71cf15747619b73bcbb6efd62e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
